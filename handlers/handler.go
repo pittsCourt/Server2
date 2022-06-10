482d6f71fed56239a164f906f0f66d34e52299d4
Court Pitts <courtney.pitts@digicert.com>
2022-06-10 11:25:28 -0600

    second commit

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,11 +1,29 @@
 package handlers
 
 import (
+	"errors"
 	"net/http"
+	"regexp"
 )
 
-func Handler(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Content-Type", "application/json; charset=utf-8")
-	res.WriteHeader(200)
-	res.Write(b)
+// type Data struct {
+// 	Id    int    //`json:"id"`
+// 	Value string //`json:"value"`
+// }
+
+func DataHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getData(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("invalid Page Title")
+	}
+	return m[2], nil // The title is the second subexpression.
 }
--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package ma
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/pittsCourt/Server2/handlers"
 )
+
+type Data struct {
+	Id    int    //`json:"id"`
+	Value string //`json:"value"`
+}
+
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	m := Data{1, "one"}
+	b, _ := json.Marshal(m)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write(b)
+	data := r.URL.Path[len("/data/"):]
+	fmt.Println(data)
+}
 
 func main() {
 	// Handling the /data/1 as a function
-	http.HandleFunc("/data/1", handlers.Handler)
+	http.HandleFunc("/data/1", DataHandler)
 
 	log.Println("Listening on port :80")
 
